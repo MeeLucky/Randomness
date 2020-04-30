334d0956bae454b6fcaacf348d44cf3b6b1610c4
MeeLock <zalipatr1@yandex.ru>
2020-04-30 19:54:31 +0300

    create inflate system

--- app/src/main/java/com/example/randomness/SecondaryActivity.java
+++ app/src/main/java/com/example/randomness/SecondaryActivity.java
@@ -1,11 +1,15 @@
 package com.example.randomness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.widget.Toast;
 
 import java.util.Objects;
+import java.util.zip.Inflater;
 
 public class SecondaryActivity extends AppCompatActivity {
 
@@ -14,9 +18,38 @@ public class SecondaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
 
-        String key = Objects.requireNonNull(getIntent().getExtras()).getString("key");
 
-        Toast.makeText(this, key, Toast.LENGTH_LONG).show();
+        LayoutInflater inflater = getLayoutInflater();
+        ConstraintLayout root = findViewById(R.id.root);
+
+        String key = Objects.requireNonNull(getIntent().getExtras()).getString("key");
+        int resource = 0;
+
+        switch (key) {
+            case "numbers":
+                resource = R.layout.numbers;
+                break;
+            case "coin":
+                resource = R.layout.coin;
+                break;
+            case "roshambo":
+                break;
+            case "cube":
+                break;
+            case "colors":
+                break;
+            case "letters":
+                break;
+            case "passwords":
+                break;
+
+            default:
+                Toast.makeText(this, "error...", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+        }
+
+        inflater.inflate(resource, root);
     }
 
 
