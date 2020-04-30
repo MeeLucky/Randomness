91455d8c19b6d98a3c8b8707c81bc06cd2ac6483
MeeLock <zalipatr1@yandex.ru>
2020-04-30 20:03:19 +0300

    create model

---
+++ app/src/main/java/com/example/randomness/Randomness.java
@@ -0,0 +1,15 @@
+package com.example.randomness;
+
+import java.util.Random;
+
+public class Randomness {
+    private static Random rand = new Random();
+
+    static int GetRandom(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        return rand.nextInt((max - min) + 1) + min;
+    }
+}
